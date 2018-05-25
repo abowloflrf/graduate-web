a1a8e415b10e23d632d15c4b842bef6c93e1e223
ruofeng <lrf0829@126.com>
2018-05-25 16:56:14 +0800

    doVote

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Post;
 
 class PostController extends Controller
@@ -31,6 +32,19 @@ class PostController extends Controller
             'msg' => "发布成功",
             "post" => $newPost
         );
-        //echo $request->authorLevel;
+    }
+
+    public function doVote(Request $request, $postid)
+    {
+        if (Post::find($postid)->increment('votes')) {
+            return response(array(
+                'status' => 1,
+                'msg' => '点赞成功'
+            ))->cookie('voted_' . $postid, 1, 1440);
+        }
+        return array(
+            'status' => 0,
+            'msg' => '点赞失败'
+        );
     }
 }
