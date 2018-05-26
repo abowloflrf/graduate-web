cd5d6c747a9393287e0e485a76465b54b60d1b31
ruofeng <lrf0829@126.com>
2018-05-27 00:26:35 +0800

    little fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,13 @@
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <link rel=icon href=/favicon.ico>
-    <title>Graduate</title>
-    <link as=style href=/css/app.8e0851f8.css rel=preload>
+    <title>graduate-vue</title>
+    <link as=style href=/css/app.ee9d40e1.css rel=preload>
     <link as=style href=/css/vendors~app.0.c19a77ea.css rel=preload>
-    <link as=script href=/js/app.1f10a95d.js rel=preload>
+    <link as=script href=/js/app.54d6ac1d.js rel=preload>
     <link as=script href=/js/vendors~app.d66939b3.js rel=preload>
     <link href=/css/vendors~app.0.c19a77ea.css rel=stylesheet>
-    <link href=/css/app.8e0851f8.css rel=stylesheet>
+    <link href=/css/app.ee9d40e1.css rel=stylesheet>
 </head>
 
 <body>
@@ -21,7 +21,7 @@
     </noscript>
     <div id=app></div>
     <script type=text/javascript src=/js/vendors~app.d66939b3.js></script>
-    <script type=text/javascript src=/js/app.1f10a95d.js></script>
+    <script type=text/javascript src=/js/app.54d6ac1d.js></script>
 </body>
 
 </html>
