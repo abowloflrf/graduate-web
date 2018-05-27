ba053536807456a0f0900b6d8f1a43e532d92c57
ruofeng <lrf0829@126.com>
2018-05-27 08:53:23 +0800

    fix timezone bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,13 @@
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <link rel=icon href=/favicon.ico>
-    <title>graduate-vue</title>
-    <link as=style href=/css/app.ee9d40e1.css rel=preload>
+    <title>毕业季 - 留下你最珍贵的回忆</title>
+    <link as=style href=/css/app.801c986f.css rel=preload>
     <link as=style href=/css/vendors~app.0.c19a77ea.css rel=preload>
-    <link as=script href=/js/app.54d6ac1d.js rel=preload>
+    <link as=script href=/js/app.a3744e26.js rel=preload>
     <link as=script href=/js/vendors~app.d66939b3.js rel=preload>
     <link href=/css/vendors~app.0.c19a77ea.css rel=stylesheet>
-    <link href=/css/app.ee9d40e1.css rel=stylesheet>
+    <link href=/css/app.801c986f.css rel=stylesheet>
 </head>
 
 <body>
@@ -21,7 +21,7 @@
     </noscript>
     <div id=app></div>
     <script type=text/javascript src=/js/vendors~app.d66939b3.js></script>
-    <script type=text/javascript src=/js/app.54d6ac1d.js></script>
+    <script type=text/javascript src=/js/app.a3744e26.js></script>
 </body>
 
 </html>
